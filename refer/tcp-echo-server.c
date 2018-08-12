0d6b52b8e405b1a5bdbb036f97fd18aa982f8df1
giangdo <dotruonggiang108@gmail.com>
2018-08-13 02:41:54 +0700

    switch to simple verions of libuv

--- refer/tcp-echo-server.c
+++ refer/tcp-echo-server.c
@@ -1,13 +1,9 @@
-#include<iostream>    //cout
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <uv.h>
-#include "json.hpp"
 
-using Json = nlohmann::json;
-
-#define DEFAULT_PORT 9090
+#define DEFAULT_PORT 7000
 #define DEFAULT_BACKLOG 128
 
 uv_loop_t *loop;
@@ -39,13 +35,6 @@ void echo_write(uv_write_t *req, int status) {
 void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
     if (nread > 0) {
         write_req_t *req = (write_req_t*) malloc(sizeof(write_req_t));
-        std::string rcvStr = std::string(buf->base);
-        std::cout << rcvStr << std::endl;
-        auto j = Json::parse(rcvStr);
-        std::string cmd = j["cmd"].get<std::string>();
-        int day = j["day"].get<int>();
-        int hour = j["hour"].get<int>();
-        std::cout << cmd << day << hour << std::endl;
         req->buf = uv_buf_init(buf->base, nread);
         uv_write((uv_write_t*) req, client, &req->buf, 1, echo_write);
         return;
@@ -69,23 +58,6 @@ void on_new_connection(uv_stream_t *server, int status) {
     uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
     uv_tcp_init(loop, client);
     if (uv_accept(server, (uv_stream_t*) client) == 0) {
-        struct sockaddr_storage peername;
-        int namelen = sizeof(peername);
-        int rc = uv_tcp_getpeername(client, (struct sockaddr*)&peername, &namelen);
-        if (rc < 0) {
-            std::cout << "Can not get peer name " << uv_strerror(rc) << std::endl;
-        }
-        char hostbuf[NI_MAXHOST], portbuf[NI_MAXSERV];
-        if (getnameinfo((struct sockaddr*)&peername, namelen , hostbuf, NI_MAXHOST, portbuf, NI_MAXSERV, 0) == 0) {
-            char ipStr[INET_ADDRSTRLEN];
-            struct sockaddr_in *s = (struct sockaddr_in *)&peername;
-            inet_ntop(AF_INET, &s->sin_addr, ipStr, sizeof(ipStr));
-            std::cout << "hostname: " << hostbuf << " ip: " << ipStr << " port:" << portbuf << std::endl;
-        } else {
-            std::cout << "peer (unknonwn hostname) connected" << std::endl;
-        }
-
-
         uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
     }
     else {
@@ -100,7 +72,6 @@ int main() {
     uv_tcp_init(loop, &server);
 
     uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
-    std::cout << "Start server at port" << std::to_string(DEFAULT_PORT) << std::endl;
 
     uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
     int r = uv_listen((uv_stream_t*) &server, DEFAULT_BACKLOG, on_new_connection);
@@ -108,6 +79,5 @@ int main() {
         fprintf(stderr, "Listen error %s\n", uv_strerror(r));
         return 1;
     }
-    int re = uv_run(loop, UV_RUN_DEFAULT);
-    return re;
+    return uv_run(loop, UV_RUN_DEFAULT);
 }
--- server/README.md
+++ server/README.md
@@ -1,14 +1,4 @@
-Thank Eli Bendersky for his amazing example of how to use libuv
-Reference: https://github.com/eliben/code-for-blog/tree/master/2017/async-socket-server
-
-Command to build:
-    + use make:
-        $cd p2p/server
-        $make
-
-    + or use long command:
-        $g++ -std=c++11 -Wall -O3 -g -DNDEBUG -pthread utils.c main.cpp -o server -lpthread -pthread -luv -Wl,-rpath=/usr/local/lib -L/usr/local/include
-
-* How to run:
-    # ./server
-    Serving on port 9090
+* reference:
+    tcp server:
+        curl -LO https://raw.githubusercontent.com/nikhilm/uvbook/master/code/tcp-echo-server/main.c
+        https://stackoverflow.com/questions/13656702/sending-and-receiving-strings-over-tcp-socket-separately
